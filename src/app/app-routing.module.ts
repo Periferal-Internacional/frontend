2995ef294163a29d8f89d6ac1cf74436dca19753
Emilio Bernal <ebernalzubieta@gmail.com>
2022-03-04 12:20:01 -0600

    modulos creados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './_modules/auth/auth.component';
+import { LandingComponent } from './_modules/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    'path': '',
+    'component': LandingComponent
+  },
+  {
+    'path': 'auth',
+    'component': AuthComponent,
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './_modules/auth/auth.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { DashboardComponent } from './_modules/dashboard/dashboard.component';
+import { VideogameComponent } from './_modules/videogame/videogame.component';
+import { AnalyticsComponent } from './_modules/analytics/analytics.component';
+import { LandingComponent } from './_modules/landing/landing.component';
+import { LeaderboardComponent } from './_components/leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    NavbarComponent,
+    DashboardComponent,
+    VideogameComponent,
+    AnalyticsComponent,
+    LandingComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule,
