04201df04f86f017dedfdfe1f0be391691841d20
Emilio Bernal <ebernalzubieta@gmail.com>
2022-03-18 10:03:19 -0600

    analytics

--- src/app/_components/leaderboard/leaderboard.component.ts
+++ src/app/_components/leaderboard/leaderboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/_services/api.service';
 
 @Component({
   selector: 'leaderboard',
@@ -6,10 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leaderboard.component.css']
 })
 export class LeaderboardComponent implements OnInit {
-
-  constructor() { }
+  tabs : string[] = [];
+  users : any[] = [];
+  percentages: number[] = [100, 75, 64, 47, 15];
+  constructor(
+    private api : ApiService
+  ) { }
 
   ngOnInit(): void {
+    this.setTab();
+  }
+
+  setTab() {
+    var temp = location.href.split("/");
+    for (var i = 0; i < 3; i++) {
+      temp.shift();
+    }
+    for (var i = 0; i < temp.length; i++) {
+      this.tabs.push(temp[i].charAt(0).toUpperCase() + temp[i].slice(1));
+    }
+    this.getData();
+  }
+
+  getData() {
+    this.api.getPipe('users').subscribe(resp => {
+      this.parse(resp);
+    }, err => {
+      console.log(err.message);
+    });
+  }
+
+  parse(information : any) {
+    if (this.tabs[0] == "Dashboard") {
+      for (var i = 0; i < 3; i++) {
+        this.users.push(information[i]);
+      }
+    } else {
+      for  (var i = 0; i < information.length; i++) {
+        this.users.push(information[i]);
+      }
+    }
+  }
+
+  getColor(i : number) {
+    if(this.percentages[i] < 50) {
+      return "red";
+    } else {
+      return "primary";
+    }
   }
 
 }
--- src/app/_modules/analytics/analytics-people/analytics-people.component.ts
+++ src/app/_modules/analytics/analytics-people/analytics-people.component.ts
@@ -1,35 +1,51 @@
-import { Component } from '@angular/core';
-
-interface Person {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../_services/api.service';
 
 @Component({
   selector: 'analytics-people',
   templateUrl: './analytics-people.component.html'
 })
-export class AnalyticsPeopleComponent {
-  listOfData: Person[] = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park'
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park'
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park'
+export class AnalyticsPeopleComponent implements OnInit {
+  users : any[] = [];
+  plants = ["Monterrey", "Leon", "CDMX", "Monterrey", "Monterrey"]
+  constructor(
+    private api : ApiService
+  ) {}
+  
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.api.getPipe("users").subscribe(resp => {
+      this.parse(resp);
+    })
+  }
+
+  parse(information : any) {
+    for (var i = 0; i < information.length; i++) {
+      this.users.push(information[i]);
     }
-  ];
+    console.log(this.users);
+  }
+  // listOfData: Person[] = [
+  //   {
+  //     key: '1',
+  //     name: 'John Brown',
+  //     age: 32,
+  //     address: 'New York No. 1 Lake Park'
+  //   },
+  //   {
+  //     key: '2',
+  //     name: 'Jim Green',
+  //     age: 42,
+  //     address: 'London No. 1 Lake Park'
+  //   },
+  //   {
+  //     key: '3',
+  //     name: 'Joe Black',
+  //     age: 32,
+  //     address: 'Sidney No. 1 Lake Park'
+  //   }
+  // ];
 }
