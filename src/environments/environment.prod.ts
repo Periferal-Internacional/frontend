e5f0a4b41cf64c59726271e606b5f9ac85d5ac60
rgg2001 <78384733+rgg2001@users.noreply.github.com>
2022-04-08 10:28:31 -0500

    solucion problema olmedo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'dragndrop-4f92c',
+    appId: '1:349903877460:web:a0a6589d063794fa1084c1',
+    storageBucket: 'dragndrop-4f92c.appspot.com',
+    locationId: 'us-west2',
+    apiKey: 'AIzaSyAFfCrfxv7G9ijtgSdDv8ar-kVgPWBeRYs',
+    authDomain: 'dragndrop-4f92c.firebaseapp.com',
+    messagingSenderId: '349903877460',
+  },
   production: true
 };
