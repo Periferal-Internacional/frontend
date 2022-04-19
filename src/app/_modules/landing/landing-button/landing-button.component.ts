1968917842eb8acf1875ab8d09a7eb1461e2ba1d
rgg2001 <78384733+rgg2001@users.noreply.github.com>
2022-04-19 10:25:26 -0500

    landing

---
+++ src/app/_modules/landing/landing-button/landing-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-button',
+  templateUrl: './landing-button.component.html',
+  styleUrls: ['./landing-button.component.css']
+})
+export class LandingButtonComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,7 @@ registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
